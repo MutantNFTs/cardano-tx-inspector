aceb6c5df9125e58a9da8c5d676cb68898775ce8
Geraldo Neto <geraldo.lopes@outlook.com>
2023-07-08 17:02:10 +0100

    feat: add outputsContainPolicyIds function

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  workerThreads: true // to handle bigint
+  workerThreads: true, // to handle bigint
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
 };
