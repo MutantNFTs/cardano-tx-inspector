ef489a9b00cda4157c655608a30886180891643a
Geraldo Neto <geraldo.lopes@outlook.com>
2023-07-08 22:47:42 +0100

    feat(getAssetDetails): ignore dot when present in asset unit

--- src/getAssetDetails.ts
+++ src/getAssetDetails.ts
@@ -2,6 +2,10 @@ import { CIP_68_ASSET_PREFIX } from "./cip68";
 import { hexToString } from "./hexToString";
 
 export const getAssetDetails = (asset: string) => {
+  if (asset.includes(".")) {
+    asset = asset.replace(".", "");
+  }
+
   const assetPolicy = asset.substring(0, 56);
   const assetName = asset.substring(56);
 
